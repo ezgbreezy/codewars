ff42260ea407605c4d1a6fed1bf1929c46019c50
gberenzy <dev@berenzy.com>
2023-02-20 13:39:54 -0500

    Completed kata

--- c/7kyu/noisy-cell-counts/ncc.c
+++ c/7kyu/noisy-cell-counts/ncc.c
@@ -5,9 +5,15 @@
 
 void cleaned_counts(size_t length, const unsigned data[length], unsigned output[length]) {
 
+    // Loop through array
     for (int i = 0; i < length; i++)
     {
-        
-    }
+        output[i] = data[i];
 
+        // Check if element is incorrect
+        if (i != 0 && output[i] < output[i - 1])
+        {
+            output[i] = output[i - 1]; // Adjust values
+        }
+    }
 }
