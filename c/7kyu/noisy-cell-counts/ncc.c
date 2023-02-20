7facc42e08ddd4dc1dba164f30c13c2505a14907
gberenzy <dev@berenzy.com>
2023-02-20 13:17:41 -0500

    Initial commit

---
+++ c/7kyu/noisy-cell-counts/ncc.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+
+//  don't allocate memory for the resulting array
+//  assign values to preallocated pointer *output
+
+void cleaned_counts(size_t length, const unsigned data[length], unsigned output[length]) {
+
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+
+}
